459641b618c964e34425b68a61c5387b7471eeaf
davidev2023 <davihebert017@gmail.com>
2023-07-15 01:29:34 -0300

    enimation

--- calendario.js
+++ calendario.js
@@ -9,17 +9,17 @@ let dia = data.getDate();
 let mes = meses[data.getMonth()];
 let ano = data.getFullYear();
 
-let ativo = document.querySelector(".semana li:nth-child("+mes+")")
+let ativo = document.querySelector(".semana li:nth-child("+diaNumero+")")
 ativo.classList.add('atual');
+
 
 let h1 = document.createElement('h1');
 h1.innerHTML = dia;
 ativo.appendChild(h1);
 
-
-let h5 = document.createElement('h5');
-h5.innerHTML = mes;
-ativo.appendChild(h5);
+let h2 = document.createElement('h2');
+h2.innerHTML = mes;
+ativo.appendChild(h2);
 
 let h3 = document.createElement('h3');
 h3.innerHTML = ano;
